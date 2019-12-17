851ecbd4269fa46afe2e306ae6207a13e7f1edea
jeramiah1017 <55998280+jeramiah1017@users.noreply.github.com>
2019-12-17 16:50:35 -0700

    finished map functionality

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,35 +1,71 @@
-import React from 'react'
-import Modal from './Games-Modal'
-
+import React from "react";
+import Modal from "./Games-Modal";
+import USAMap from "react-usa-map";
+import styled from 'styled-components'
 
 class Map extends React.Component {
-    state = {
-        modalDisplay: false,
-        st: ''
-    }
-    
-    showModal(state){
-        this.setState({
-            modalDisplay: true,
-            st: state
-        })
-    }
-    
-    render(){
+  state = {
+    modalDisplay: false,
+    st: ""
+  };
 
-        return(
-            <div>
-            
-            <h1>Map</h1>
-            
-            <button
-            onClick={ ()=> this.showModal('UT') }
-            >Utah</button>
-
-            {this.state.modalDisplay && <Modal st={this.state.st} />}
-            </div>
-    )
+//   toggleModal = () => {
+//     this.setState({
+//       modalDisplay: !this.state.modalDisplay
+//     });
+  
+  closeMenu = event => {
+      if (!this.Modal.contains(event.target)) {
+        console.log('close menu is running')
+      this.setState({ modalDisplay: false }, () => {
+        document.removeEventListener("click", this.closeMenu);
+      });
     }
+  };
+  showMenu = () => {
+      console.log('menu is working')
+      this.setState({modalDisplay:true}, () => {
+          document.addEventListener('click', this.closeMenu)
+      })
+  };
+
+  mapHandler = event => {
+    console.log(event.target.dataset.name);
+    this.setState({
+      st: event.target.dataset.name
+    });
+    this.showMenu();
+  };
+  assignElement = (el) => {
+    this.Modal = el
+  }
+
+  render() {
+    return (
+      <MapPage>
+        <input placeholder="username" />
+        <input placeholder="password" />
+        <button>confirm</button>
+        <div className="App">
+          <USAMap onClick={this.mapHandler} />
+          {/* <USAMap onClick={() => {this.toggleModal(); this.mapHandler()}} /> */}
+          {this.state.modalDisplay ? (
+            <Modal closeMenu={this.hideMenu} st={this.state.st} assignElement={this.assignElement} />
+          ) : null}
+        </div>
+      </MapPage>
+    );
+  }
 }
 
-export default Map
+
+export default Map;
+
+const MapPage = styled.div `
+path:hover {
+    opacity: 0.60;
+    cursor:  pointer;
+    transform: scale(1.005);
+    z-index: 3;}
+    
+    `

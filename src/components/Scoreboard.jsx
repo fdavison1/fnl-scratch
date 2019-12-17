3113fe8d2b9871239dc0718ebcf94bb278c07184
Chazensign <chazensign@gmail.com>
2019-12-17 12:22:37 -0700

    added helmet components and minor styling

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,31 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
+import Helmet from './Helmet'
 
 const Wrapper = styled.div`
-border: solid 1px #999999
-width: 1000px
-margin: 25px
-box-sizing: border-box`
+  width: 600px;
+  margin: 25px;
+  background: white;
+  border-radius: 7px;
+  box-sizing: border-box;
+  .school-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .team, .teams{
+    display: flex;
+    justify-content: space-evenly;
+  }
+`
 
-export default class Scoreboard extends React.Component{
-    state = {}
+export default class Scoreboard extends React.Component {
+  state = {}
 
-    render(){
-        const h= this.props.game.home
-        const a = this.props.game.away
-        return (
-            <Wrapper>
-
-                Home
-                School: {h.school}
-                Mascot: {h.mascot}
-                Color: {h.color}
-                <br/>
-                Away
-                School: {a.school}
-                Mascot: {a.mascot}
-                Color: {a.color}
-            </Wrapper>
-        )
-    }
+  render() {
+    const h = this.props.game.home
+    const a = this.props.game.away
+    return (
+      <Wrapper>
+          <div className='teams'>
+        <div className='team'>
+          <Helmet color1={h.color} />
+          <div className='school-info'>
+            <h2>Home School: {h.school}</h2>
+            <h2>Mascot: {h.mascot}</h2>
+          </div>
+        </div>
+        <br />
+        <div className='team'>
+          <div className='school-info'>
+            <h2>Away School: {a.school}</h2>
+            <h2>Mascot: {a.mascot}</h2>
+          </div>
+          <Helmet rightHelmet={true} color1={a.color} />
+        </div>
+        </div>
+      </Wrapper>
+    )
+  }
 }

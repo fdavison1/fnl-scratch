c8f12c15b690f05fbce0ba400bdb48b8e41b4d37
fdavison1 <fred.noreply@gmail.com>
2019-12-19 02:05:31 -0700

    drives component

--- src/components/Drives.jsx
+++ src/components/Drives.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import Drive from './Drive'
 
 const Wrapper = styled.div`
-margin-left: 150px
 border: 1px solid black
-height: 300px
-width: 350px`
+border-radius: 7px;
+margin-left: 25px;
+height: 400px
+width: 475px`
 
 export default class Drives extends React.Component{
     state={
 
     }
     render(){
+        const { drives } = this.props.game
         return(
             <Wrapper>
                 <h1>Drives!</h1>
+
+                {drives.length === 0 ? 'No drives yet' :
+                <div className='drives'>
+                    
+                    {drives.map(drive => <Drive key={drive.index} drive={drive}/>)}
+
+
+                </div>}
+
+
             </Wrapper>
         )
     }
